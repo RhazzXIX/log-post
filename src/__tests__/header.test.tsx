277a56acb5d7ed2b3155eb67b6396e1f014a5e46
RhazzXIX <jm21tonio@gmail.com>
2023-08-01 19:59:42 +0800

    Add conditional for buttons in Header component.
    Added a conditional render of buttons if the user is logged in.

--- src/__tests__/header.test.tsx
+++ src/__tests__/header.test.tsx
@@ -1,11 +1,22 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Header from "@/component/header";
+
+const user: IUser = {
+  id: "125id",
+  name: "test user",
+  isAuthor: true,
+};
 
 describe("Header component", () => {
   it("Renders on screen", () => {
     const { container } = render(<Header />);
 
     expect(container).toMatchSnapshot();
+  });
+  it("Shows appropriate buttons when the user is signed in", () => {
+    render(<Header user={user} />);
+
+    expect(screen.getByText("Profile")).toBeInTheDocument();
   });
 });
--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
 const Header = ({ user }: { user?: IUser }) => {
+  // Render jsx element.
   return (
     <header>
       <div className="widthLimiter">
         <Link href={"/"}>
           <h1>Log Pose</h1>
         </Link>
-        <button>Log in</button>
-        <button>Sign up</button>
+        {/* conditional for buttons if user is logged in */}
+        {user ? (
+          <Link href={"/user"} className="linkBtn">
+            Profile
+          </Link>
+        ) : (
+          <>
+            <button>Log in</button>
+            <button>Sign up</button>
+          </>
+        )}
       </div>
     </header>
   );
