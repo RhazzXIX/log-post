e64d6e41d8f99bfbe48014411f18bb42adcb3b23
RhazzXIX <jm21tonio@gmail.com>
2023-08-02 16:44:32 +0800

    Add logic to show different form
    Added a logic to show a sign-up form version sign-up is rendered.

--- src/__tests__/UserForm.test.tsx
+++ src/__tests__/UserForm.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import UserForm from "@/component/UserForm";
 
@@ -6,5 +6,14 @@ describe("UserForm component", () => {
   it("Renders on screen", () => {
     const { container } = render(<UserForm version={"log-in"} />);
     expect(container).toMatchSnapshot();
+  });
+  it("Shows the confirm password input if the versions is sign-up", () => {
+    const { rerender } = render(<UserForm version="log-in" />);
+
+    expect(screen.queryByLabelText("Confirm password")).not.toBeInTheDocument();
+
+    rerender(<UserForm version="sign-up" />);
+
+    expect(screen.queryByLabelText("Confirm password:")).toBeInTheDocument();
   });
 });
--- src/component/UserForm.tsx
+++ src/component/UserForm.tsx
@@ -1,6 +1,6 @@
 export default function UserForm({ version }: { version: string }) {
   return (
-    <form action="">
+    <form action="" method="POST">
       <label htmlFor="username">
         Username:
         <input type="text" id="username" name="username" />
@@ -9,6 +9,17 @@ export default function UserForm({ version }: { version: string }) {
         Password:
         <input type="password" name="password" id="password" />
       </label>
+      {/* Logic to show password confirmation for version sign-up */}
+      {version === "sign-up" && (
+        <label htmlFor="passwordConfirmation">
+          Confirm password:
+          <input
+            type="password"
+            name="passwordConfirmation"
+            id="passwordConfirmation"
+          />
+        </label>
+      )}
       <button type="submit">Submit</button>
     </form>
   );
