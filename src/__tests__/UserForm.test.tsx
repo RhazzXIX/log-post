dbff25c3ebcdba085d2a293946b26bd1fe1da23f
RhazzXIX <jm21tonio@gmail.com>
2023-08-05 17:11:58 +0800

    Add logic for form submission

--- src/__tests__/UserForm.test.tsx
+++ src/__tests__/UserForm.test.tsx
@@ -1,12 +1,38 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import UserForm from "@/component/UserForm";
+import userevent from "@testing-library/user-event";
+
+const fetch = (global.fetch = jest.fn());
+
+const user = userevent.setup();
+
+const userInfo: IUser | undefined = {
+  id: "125id",
+  name: "testUser1234",
+  isAuthor: true,
+};
+
+const userInput = {
+  username: "testUser1234",
+  password: "testPass1234",
+  passwordConfirmation: "testPass1234",
+};
+
+const sendData = {
+  mode: "cors",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
 
 describe("UserForm component", () => {
   it("Renders on screen", () => {
     const { container } = render(<UserForm version={"log-in"} />);
     expect(container).toMatchSnapshot();
   });
+
   it("Shows the confirm password input if the versions is sign-up", () => {
     const { rerender } = render(<UserForm version="log-in" />);
 
@@ -15,5 +41,78 @@ describe("UserForm component", () => {
     rerender(<UserForm version="sign-up" />);
 
     expect(screen.queryByLabelText("Confirm password:")).toBeInTheDocument();
+  });
+
+  describe("User inputs and form submission", () => {
+    beforeEach(() => {
+      fetch.mockClear();
+    });
+
+    it("Let's the user sign-up", async () => {
+      (fetch as jest.Mock).mockResolvedValue({
+        json: () => Promise.resolve({ message: "Response ok", userInfo }),
+      });
+
+      render(<UserForm key={"sign-up"} version={"sign-up"} />);
+
+      const usernameInput = screen.getByLabelText(/Username/);
+      const passwordInput = screen.getByLabelText("Password:");
+      const submitBtn = screen.getByRole("button", { name: "Submit" });
+      const passwordConfirmationInput = screen.getByLabelText(/confirm/i);
+
+      await user.type(usernameInput, userInput.username);
+      await user.type(passwordInput, userInput.password);
+      await user.type(
+        passwordConfirmationInput,
+        userInput.passwordConfirmation
+      );
+
+      expect(usernameInput).toHaveValue("testUser1234");
+      expect(passwordInput).toHaveValue("testPass1234");
+      expect(passwordConfirmationInput).toHaveValue("testPass1234");
+
+      await user.click(submitBtn);
+
+      expect(fetch).toHaveBeenCalled();
+      expect(fetch.mock.calls[0][0]).toMatch(
+        "https://blog-api-86j6.onrender.com/users"
+      );
+      expect(fetch.mock.calls[0][1]).toEqual({
+        body: JSON.stringify({ ...userInput }),
+        ...sendData,
+      });
+    });
+
+    it("Let's the user log-in", async () => {
+      (fetch as jest.Mock).mockResolvedValue({
+        json: () => Promise.resolve({ message: "Response ok", userInfo }),
+      });
+
+      render(<UserForm key={"log-in"} version={"log-in"} />);
+
+      const usernameInput = screen.getByLabelText(/Username/);
+      const passwordInput = screen.getByLabelText("Password:");
+      const submitBtn = screen.getByRole("button", { name: "Submit" });
+
+      await user.type(usernameInput, userInput.username);
+      await user.type(passwordInput, userInput.password);
+
+      expect(usernameInput).toHaveValue("testUser1234");
+      expect(passwordInput).toHaveValue("testPass1234");
+
+      await user.click(submitBtn);
+
+      expect(fetch).toHaveBeenCalled();
+      expect(fetch.mock.calls[0][0]).toMatch(
+        "https://blog-api-86j6.onrender.com/session"
+      );
+      expect(fetch.mock.calls[0][1]).toEqual({
+        body: JSON.stringify({
+          username: userInput.username,
+          password: userInput.password,
+        }),
+        ...sendData,
+      });
+    });
   });
 });
--- src/component/UserForm.tsx
+++ src/component/UserForm.tsx
@@ -1,13 +1,87 @@
+"use client";
+import { ChangeEvent, FormEvent, useState } from "react";
+
 export default function UserForm({ version }: { version: string }) {
+  // Component state and variables.
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const url =
+    version === "sign-up"
+      ? "https://blog-api-86j6.onrender.com/users"
+      : "https://blog-api-86j6.onrender.com/session";
+
+  // User input handlers.
+  const handleUsernameInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const handlePasswordConfirmation = (e: ChangeEvent<HTMLInputElement>) => {
+    setPasswordConfirmation(e.target.value);
+  };
+
+  // Form submission handler.
+  const handleSubmitForm = async (e: FormEvent) => {
+    e.preventDefault();
+    
+    // Form data to be sent
+    const formData: {
+      username: string;
+      password: string;
+      passwordConfirmation?: string;
+    } = {
+      username,
+      password,
+    };
+
+    // Logic to add password confirmation incase of sign-up version.
+    if (version === "sign-up")
+      formData.passwordConfirmation = passwordConfirmation;
+    
+    // Fetch data
+    const apiData = await fetch(url, {
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((error: Error) => {
+        throw new Error(error.message);
+      });
+    
+    console.log(apiData);
+  };
+
   return (
-    <form action="" method="POST">
+    <form action="" method="POST" onSubmit={handleSubmitForm}>
       <label htmlFor="username">
         Username:
-        <input type="text" id="username" name="username" />
+        <input
+          type="text"
+          id="username"
+          name="username"
+          onChange={handleUsernameInput}
+          value={username}
+        />
       </label>
       <label htmlFor="password">
         Password:
-        <input type="password" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={handlePasswordInput}
+          value={password}
+        />
       </label>
       {/* Logic to show password confirmation for version sign-up */}
       {version === "sign-up" && (
@@ -17,6 +91,8 @@ export default function UserForm({ version }: { version: string }) {
             type="password"
             name="passwordConfirmation"
             id="passwordConfirmation"
+            onChange={handlePasswordConfirmation}
+            value={passwordConfirmation}
           />
         </label>
       )}
