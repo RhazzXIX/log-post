b25cd56e58a09ed97dc8f48d3404b6c90feee13b
RhazzXIX <jm21tonio@gmail.com>
2023-08-12 17:33:41 +0800

    Add interfaces IPostContent & IPost

--- src/custom-types/types.ts
+++ src/custom-types/types.ts
@@ -13,3 +13,19 @@ interface IFormErrMessage {
 interface signUpOkResponse extends IUser {
   message: string;
 }
+
+interface IPostContent {
+  _id: string;
+  headerImg?: { data: Buffer; contentType: string };
+  title: string;
+  text: string;
+}
+
+interface IPost {
+  _id: string;
+  content: IPostContent[];
+  isPublished: boolean;
+  publishedDate?: Date;
+  author: { id: string; name: string };
+  totalComments: number;
+}
