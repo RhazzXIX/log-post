819542fee737d9d731c24e9475e988ebf89456a8
RhazzXIX <jm21tonio@gmail.com>
2023-08-29 08:18:43 +0800

    Add logic to limit rendered posts

--- src/component/BlogPosts.tsx
+++ src/component/BlogPosts.tsx
@@ -4,22 +4,27 @@ import Link from "next/link";
 export default function BlogPosts({
   posts,
   sortBy,
+  isMain,
 }: {
   posts: IPost[];
   sortBy: string;
+  isMain: boolean;
 }) {
   let sortedPosts: IPost[] | undefined;
+  let limit = isMain ? posts.length : 5 ;
   if (sortBy === "latest") {
-    sortedPosts = [...posts].sort((a, b) => {
-      if (a.publishedDate && b.publishedDate) {
-        if (a.publishedDate > b.publishedDate) return -1;
-        if (a.publishedDate < b.publishedDate) return 1;
-        if (a.publishedDate === b.publishedDate) return 0;
-      }
-      if (a.publishedDate && b.publishedDate === undefined) return -1;
-      if (a.publishedDate === undefined && b.publishedDate) return 1;
-      return 0;
-    });
+    sortedPosts = [...posts]
+      .sort((a, b) => {
+        if (a.publishedDate && b.publishedDate) {
+          if (a.publishedDate > b.publishedDate) return -1;
+          if (a.publishedDate < b.publishedDate) return 1;
+          if (a.publishedDate === b.publishedDate) return 0;
+        }
+        if (a.publishedDate && b.publishedDate === undefined) return -1;
+        if (a.publishedDate === undefined && b.publishedDate) return 1;
+        return 0;
+      })
+      .slice(0, limit);
   } else if (sortBy === "popular") {
     sortedPosts = [...posts]
       .sort((a, b) => {
@@ -27,23 +32,26 @@ export default function BlogPosts({
         if (a.totalComments < b.totalComments) return 1;
         return 0;
       })
-      .slice(0, 6);
+      .slice(0, limit);
   }
-
-  console.log(sortedPosts);
 
   return (
     <ul>
       {sortedPosts && sortedPosts.length > 0 ? (
-        sortedPosts.map((posts) =>
-          posts.content.length > 0 ? (
-            <li key={posts._id}>
-              <Link href={`/post/${posts._id}`}>{posts.content[0].title}</Link>
+        sortedPosts.map((post) =>
+          post.content.length > 0 ? (
+            <li key={post._id}>
+              <Link href={`/posts/${post._id}`}>{post.content[0].title}</Link>
             </li>
           ) : null
         )
       ) : (
         <Loading />
+      )}
+      {isMain === false && (
+        <li>
+          <Link href={"/posts"}>View all posts</Link>
+        </li>
       )}
     </ul>
   );
